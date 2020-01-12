594cbdf5720bb6d1ed6d410237416af49fa58004
Brady Dean <2bdkid@gmail.com>
2020-01-12 15:55:15 -0500

    moved fn color to a closure

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,6 @@
 use std::char;
+use std::iter;
+use std::slice;
 
 #[aoc_generator(day8)]
 pub fn input_generator(input: &str) -> Vec<u32> {
@@ -23,19 +25,16 @@ pub fn solve_part1(input: &[u32]) -> i32 {
     ones * twos
 }
 
-fn color<'a, I>(pixel: I) -> char
-where
-    I: Iterator<Item = &'a u32>,
-{
-    pixel
-        .skip_while(|l| **l == 2)
-        .next()
-        .map(|c| char::from_digit(*c, 10).unwrap())
-        .unwrap()
-}
-
 #[aoc(day8, part2)]
 pub fn solve_part2(input: &[u32]) -> String {
+    let color = |pixel: iter::StepBy<iter::Skip<slice::Iter<u32>>>| {
+        pixel
+            .skip_while(|l| **l == 2)
+            .next()
+            .map(|c| char::from_digit(*c, 10).unwrap())
+            .unwrap()
+    };
+
     let pixel = |n| input.iter().skip(n).step_by(25 * 6);
     (0..(25 * 6)).map(|px| color(pixel(px))).collect()
 }
